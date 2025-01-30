427f889ca0d6afdf8a9deeba83bc07d1153d83d1
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-01-30 00:52:44 -0500

    scaffold

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,10 @@ import { DatabaseModule } from './database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DB_HOST, DB_NAME, DB_PASS, DB_USER } from './app.const';
 import { databaseEntities } from './database/database.const';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { GroupModule } from './group/group.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
@@ -17,7 +21,11 @@ import { databaseEntities } from './database/database.const';
       entities: databaseEntities,
       synchronize: false,
     }),
+    ArticleModule,
+    AuthModule,
     DatabaseModule,
+    UserModule,
+    GroupModule,
   ],
   controllers: [AppController],
   providers: [AppService],
