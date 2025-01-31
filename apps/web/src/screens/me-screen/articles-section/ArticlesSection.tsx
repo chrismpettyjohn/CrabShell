d8f2c429d522b236b806eb07a0d78ed5f3c16637
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-01-30 21:26:19 -0500

    feat: articles home page

--- apps/web/src/screens/me-screen/articles-section/ArticlesSection.tsx
+++ apps/web/src/screens/me-screen/articles-section/ArticlesSection.tsx
@@ -1,59 +1,27 @@
+import { articleService, ArticleWire } from "@crabshell/client";
+import { createSignal, onMount } from "solid-js";
+
 export function ArticlesSection() {
+  const [articles, setArticles] = createSignal<ArticleWire[]>([]);
+
+  onMount(async () => {
+    const response = await articleService.getAll();
+    setArticles(response.slice(0, 4));
+  });
+
   return (
     <div class="news-articles">
       <div class="container">
-        <div
-          class="card"
-          style="
-                background-image: url(https://swfs.habcrab.com/c_images/web_promo/airplane_webpromo.png);
-              "
-        >
-          <div class="card-content">
-            <div class="label">Client</div>
-            <h3>Are you crabby?</h3>
-            <p>i'm a crab living a good crab life</p>
-            <span>Jan 25, 2025</span>
+        {articles().map((_) => (
+          <div class="card" style={`background-image: url(${_.imageUrl});`}>
+            <div class="card-content">
+              <div class="label">Client</div>
+              <h3>{_.name}</h3>
+              <p>{_.description}</p>
+              <span>{_.createdAt}</span>
+            </div>
           </div>
-        </div>
-        <div
-          class="card"
-          style="
-                background-image: url(https://swfs.habcrab.com/c_images/web_promo/ABBOBADOS_P01.png);
-              "
-        >
-          <div class="card-content">
-            <div class="label">Client</div>
-            <h3>Room of the week</h3>
-            <p>i'm a crab living a good crab life</p>
-            <span>Jan 25, 2025</span>
-          </div>
-        </div>
-        <div
-          class="card"
-          style="
-                background-image: url(https://swfs.habcrab.com/c_images/web_promo/bubblejuice_largepromo.png);
-              "
-        >
-          <div class="card-content">
-            <div class="label">Client</div>
-            <h3>Staff Applications</h3>
-            <p>i'm a crab living a good crab life</p>
-            <span>Jan 25, 2025</span>
-          </div>
-        </div>
-        <div
-          class="card"
-          style="
-                background-image: url(https://swfs.habcrab.com/c_images/web_promo/habbofun_battlefield_fansiteweek_2.png);
-              "
-        >
-          <div class="card-content">
-            <div class="label">Client</div>
-            <h3>HabCrab Grand Opening</h3>
-            <p>i'm a crab living a good crab life</p>
-            <span>Jan 25, 2025</span>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
