e8d2f04d06bb237edc7b4753322625018825e7a1
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-01-30 22:47:54 -0500

    feat: logout

--- apps/web/src/screens/logout-screen/LogoutScreen.tsx
+++ apps/web/src/screens/logout-screen/LogoutScreen.tsx
@@ -1,8 +1,18 @@
-import type { Component } from "solid-js";
+import { onMount, type Component } from "solid-js";
 import { GuardUser } from "../../components/guard-user/GuardUser";
 import { SiteTitle } from "../../components/site-title/SiteTitle";
+import { authService } from "@crabshell/client";
+import { redirect } from "@solidjs/router";
+import { useAuth } from "../../context/AuthContext";
 
 const LogoutScreen: Component = () => {
+  const { setUser } = useAuth();
+  onMount(async () => {
+    await authService.logout();
+    setUser(null);
+    return redirect("/login");
+  });
+
   return (
     <GuardUser>
       <SiteTitle>Logout</SiteTitle>
