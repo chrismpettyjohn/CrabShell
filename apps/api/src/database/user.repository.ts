466033ed708429c031883c8e1ff608bee846c078
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-01-31 16:55:06 -0500

    fix: register

--- apps/api/src/database/user.repository.ts
+++ apps/api/src/database/user.repository.ts
@@ -2,8 +2,7 @@ import Random from 'randomstring';
 import { UserEntity } from './user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { hashSync } from 'bcryptjs';
-import { Repository, FindOptionsWhere } from 'typeorm';
+import { Repository } from 'typeorm';
 import { BaseRepository } from './base.repository';
 
 @Injectable()
@@ -13,24 +12,6 @@ export class UserRepository extends BaseRepository<UserEntity> {
   ) {
     super(profileRepo);
   }
-
-  async create(newEntity: Omit<UserEntity, 'id'>): Promise<UserEntity> {
-    return super.create({
-      ...newEntity,
-      password: newEntity.password && hashSync(newEntity.password),
-    });
-  }
-
-  async update(
-    conditions: FindOptionsWhere<UserEntity>,
-    changes: Partial<UserEntity>,
-  ): Promise<void> {
-    return super.update(conditions, {
-      ...changes,
-      password: changes.password ? hashSync(changes.password) : undefined,
-    });
-  }
-
   async generateSSO(userID: number): Promise<string> {
     const gameSSO: string = 'imagine_' + Random.generate(50) + '_' + userID;
     await this.update({ id: userID }, { gameSSO });
