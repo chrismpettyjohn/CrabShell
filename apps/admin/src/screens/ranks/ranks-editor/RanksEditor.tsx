284f9e174209171430abe5b787b97b4153dbaa30
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-02-08 23:22:00 -0500

    feat: rank editor

--- apps/admin/src/screens/ranks/ranks-editor/RanksEditor.tsx
+++ apps/admin/src/screens/ranks/ranks-editor/RanksEditor.tsx
@@ -1,5 +1,9 @@
-import { AdminRankCreateParams, AdminRankWire } from "@crabshell/admin-client";
-import { createSignal } from "solid-js";
+import {
+  AdminRankCreateParams,
+  AdminRankWire,
+  RankScope,
+} from "@crabshell/admin-client";
+import { createSignal, createEffect } from "solid-js";
 import { BADGE_BASE_URL, BADGE_EXT } from "../../../App.const";
 
 export interface RankEditorProps {
@@ -8,14 +12,37 @@ export interface RankEditorProps {
 }
 
 export function RanksEditor({ defaultRank, onSave }: RankEditorProps) {
+  const initialScopes = defaultRank?.()?.scopes ?? {};
   const [dto, setDTO] = createSignal<AdminRankCreateParams>({
     name: defaultRank?.()?.name ?? "",
     badgeCode: defaultRank?.()?.badgeCode ?? "",
+    scopes: { ...(initialScopes as any) },
   });
+
+  function prettifyScopeName(scope: string) {
+    return scope
+      .replace(/([A-Z])/g, " $1")
+      .replace(/^./, (str) => str.toUpperCase())
+      .trim();
+  }
 
   function onChange(event: InputEvent) {
     const { name, value } = event.currentTarget as HTMLInputElement;
     setDTO((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function onBlur() {
+    onSave(dto());
+  }
+
+  function onToggleScope(scope: RankScope) {
+    setDTO((prev) => ({
+      ...prev,
+      scopes: {
+        ...prev.scopes,
+        [scope]: !prev.scopes[scope],
+      },
+    }));
   }
 
   function onSubmit(event: Event) {
@@ -23,12 +50,21 @@ export function RanksEditor({ defaultRank, onSave }: RankEditorProps) {
     onSave(dto());
   }
 
+  createEffect(() => {
+    onSave(dto());
+  });
+
   return (
     <form onSubmit={onSubmit}>
-      <label>Name</label>
-      <input name="name" value={dto().name} onInput={onChange} type="text" />
-
-      <label>Badge</label>
+      <h2>Name</h2>
+      <input
+        name="name"
+        value={dto().name}
+        onInput={onChange}
+        onBlur={onBlur}
+        type="text"
+      />
+      <h2>Badge</h2>
       <div style="display:flex;gap:14px;justify-content:space-between;align-items:center;width:100%;">
         <img
           src={`${BADGE_BASE_URL}/${dto().badgeCode}${BADGE_EXT}`}
@@ -38,12 +74,25 @@ export function RanksEditor({ defaultRank, onSave }: RankEditorProps) {
           name="badgeCode"
           value={dto().badgeCode}
           onInput={onChange}
+          onBlur={onBlur}
           type="text"
           style="width:100%;"
         />
       </div>
-      <div style="display:flex;justify-content:flex-end;width:100%;margin-top:auto;">
-        <button type="submit">Save</button>
+
+      <h2>Scopes</h2>
+      <div style="display: flex; flex-direction: column; flex-wrap: wrap; gap: 10px;">
+        {Object.keys(dto().scopes).map((scope) => (
+          <div style="display: flex; align-items: center; gap: 8px;">
+            <input
+              type="checkbox"
+              checked={dto().scopes[scope as RankScope]}
+              onChange={() => onToggleScope(scope as RankScope)}
+              style="margin-top:4px;"
+            />
+            <h3>{prettifyScopeName(scope)}</h3>
+          </div>
+        ))}
       </div>
     </form>
   );
