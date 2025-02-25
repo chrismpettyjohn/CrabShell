12a94d9583c5be257b48dee42f3356e24d00b939
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-02-24 22:40:41 -0500

    fix: use diff ports so it can run locally with emu and nitro

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -6,24 +6,16 @@ export default defineConfig(({ mode }) => {
 
   return {
     define: {
-      "process.env.API_BASE_URL": JSON.stringify(
-        env.VITE_API_BASE_URL || "/api"
-      ),
-      "process.env.IMAGER_BASE_URL": JSON.stringify(
-        env.VITE_IMAGER_BASE_URL || ""
-      ),
-      "process.env.BADGE_BASE_URL": JSON.stringify(
-        env.VITE_BADGE_BASE_URL || ""
-      ),
+      "process.env.API_BASE_URL": JSON.stringify(env.VITE_API_BASE_URL || "/api"),
+      "process.env.IMAGER_BASE_URL": JSON.stringify(env.VITE_IMAGER_BASE_URL || ""),
+      "process.env.BADGE_BASE_URL": JSON.stringify(env.VITE_BADGE_BASE_URL || ""),
       "process.env.BADGE_EXT": JSON.stringify(env.VITE_BADGE_EXT || ""),
       "process.env.MAIN_SITE_URL": JSON.stringify(env.VITE_MAIN_SITE_URL || ""),
-      "process.env.GUEST_REDIRECT_URL": JSON.stringify(
-        env.VITE_GUEST_REDIRECT_URL || ""
-      ),
+      "process.env.GUEST_REDIRECT_URL": JSON.stringify(env.VITE_GUEST_REDIRECT_URL || ""),
     },
     plugins: [solidPlugin()],
     server: {
-      port: 3002,
+      port: 4002,
     },
     build: {
       target: "esnext",
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,39 +6,21 @@ export default defineConfig(({ mode }) => {
 
   return {
     define: {
-      "process.env.API_BASE_URL": JSON.stringify(
-        env.VITE_API_BASE_URL || "/api"
-      ),
-      "process.env.IMAGER_BASE_URL": JSON.stringify(
-        env.VITE_IMAGER_BASE_URL || ""
-      ),
+      "process.env.API_BASE_URL": JSON.stringify(env.VITE_API_BASE_URL || "/api"),
+      "process.env.IMAGER_BASE_URL": JSON.stringify(env.VITE_IMAGER_BASE_URL || ""),
       "process.env.NITRO_URL": JSON.stringify(env.VITE_NITRO_URL || ""),
-      "process.env.BADGE_BASE_URL": JSON.stringify(
-        env.VITE_BADGE_BASE_URL || ""
-      ),
+      "process.env.BADGE_BASE_URL": JSON.stringify(env.VITE_BADGE_BASE_URL || ""),
       "process.env.SITE_NAME": JSON.stringify(env.VITE_SITE_NAME || "HabCrab"),
-      "process.env.USER_OF_THE_WEEK_ID": JSON.stringify(
-        env.VITE_USER_OF_THE_WEEK_ID || "-1"
-      ),
-      "process.env.USER_OF_THE_WEEK_TEXT": JSON.stringify(
-        env.VITE_USER_OF_THE_WEEK_TEXT || ""
-      ),
-      "process.env.ADMIN_PANEL_URL": JSON.stringify(
-        env.VITE_ADMIN_PANEL_URL || ""
-      ),
-      "process.env.GUEST_REDIRECT_URL": JSON.stringify(
-        env.VITE_GUEST_REDIRECT_URL || ""
-      ),
-      "process.env.FINDRETROS_GUARD_ENABLED": JSON.stringify(
-        env.VITE_FINDRETROS_GUARD_ENABLED || ""
-      ),
-      "process.env.DISCORD_INVITE_URL": JSON.stringify(
-        env.VITE_DISCORD_INVITE_URL || ""
-      ),
+      "process.env.USER_OF_THE_WEEK_ID": JSON.stringify(env.VITE_USER_OF_THE_WEEK_ID || "-1"),
+      "process.env.USER_OF_THE_WEEK_TEXT": JSON.stringify(env.VITE_USER_OF_THE_WEEK_TEXT || ""),
+      "process.env.ADMIN_PANEL_URL": JSON.stringify(env.VITE_ADMIN_PANEL_URL || ""),
+      "process.env.GUEST_REDIRECT_URL": JSON.stringify(env.VITE_GUEST_REDIRECT_URL || ""),
+      "process.env.FINDRETROS_GUARD_ENABLED": JSON.stringify(env.VITE_FINDRETROS_GUARD_ENABLED || ""),
+      "process.env.DISCORD_INVITE_URL": JSON.stringify(env.VITE_DISCORD_INVITE_URL || ""),
     },
     plugins: [solidPlugin()],
     server: {
-      port: 3000,
+      port: 4001,
     },
     build: {
       target: "esnext",
